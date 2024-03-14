30dd4c39a3a9651e636868107faad842487a704a
Txor <txorlings@gmail.com>
2024-03-14 19:08:10 +0100

    Initial project

---
+++ src/main/java/org/txor/pokemonhackingsession/PokemonhackingsessionApplication.java
@@ -0,0 +1,13 @@
+package org.txor.pokemonhackingsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonhackingsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonhackingsessionApplication.class, args);
+	}
+
+}
