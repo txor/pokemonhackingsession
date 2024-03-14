57b3808086e6edff9831e5ee0c7a63aa88706946
Txor <txorlings@gmail.com>
2024-03-14 19:08:10 +0100

    Initial project

---
+++ src/test/java/org/txor/pokemonhackingsession/PokemonhackingsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.txor.pokemonhackingsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonhackingsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
